e6f080fe26a7ba13cf4181eeacfde779900f9fb9
itggot-karl-hellerstedt <43438073+itggot-karl-hellerstedt@users.noreply.github.com>
2019-05-02 14:55:40 +0200

    added css and html

---
+++ site/js/main.js
@@ -0,0 +1,9 @@
+function jobbish(event){
+    let enddd = document.querySelector("#tro")
+    enddd.classList.toggle("enddd")
+}
+
+function jobbisk(event){
+    let enddd = document.querySelector("#dro")
+    enddd.classList.toggle("enddd")
+}
