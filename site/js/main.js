7dc6f00c099459e8e63ede5b352960e800fbeefb
itggot-karl-hellerstedt <43438073+itggot-karl-hellerstedt@users.noreply.github.com>
2019-05-09 15:47:50 +0200

    changes in jS and css and index.html files.

--- site/js/main.js
+++ site/js/main.js
@@ -1,9 +1,6 @@
 function jobbish(event){
     let enddd = document.querySelector("#tro")
     enddd.classList.toggle("enddd")
-}
-
-function jobbisk(event){
-    let enddd = document.querySelector("#dro")
-    enddd.classList.toggle("enddd")
+    let endd = document.querySelector("#dro")
+    endd.classList.toggle("enddd")
 }
